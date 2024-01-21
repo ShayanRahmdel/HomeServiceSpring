d70bab823a03f13f78cc350a5d8861268b422f9f
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:22:47 +0330

    create HomeServiceSpringApplication

---
+++ src/main/java/com/shayanr/HomeServiceSpring/HomeServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shayanr.HomeServiceSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceSpringApplication.class, args);
+	}
+
+}
