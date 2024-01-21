5226c47be3c3d319fa1d1550a2e4012def09b47e
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:23:33 +0330

    create OrderStatus

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/enumration/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.shayanr.HomeServiceSpring.entity.enumration;
+
+public enum OrderStatus {
+
+    WAITING_EXPERT_SUGESTION,
+    WAITING_EXPERT_SELECTION,
+    WAITING_FOR_THE_SPECIALIST_TO_COME,
+    WORK_BEING,
+    WORK_DONE,
+    PAID
+
+}
