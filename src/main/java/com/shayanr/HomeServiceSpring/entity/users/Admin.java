fae22d7e03f4e2b75032a9f25ad6ef5e2627c19d
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:19:09 +0330

    create Admin

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/users/Admin.java
@@ -0,0 +1,14 @@
+package com.shayanr.HomeServiceSpring.entity.users;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Entity
+@Getter
+@Setter
+public class Admin extends User {
+}
