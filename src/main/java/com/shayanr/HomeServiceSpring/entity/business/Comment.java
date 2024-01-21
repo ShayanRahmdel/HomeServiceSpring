7712262f6f4e2b39fec0fece8c6bc30cb5f34e87
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:19:51 +0330

    create Comment

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/business/Comment.java
@@ -0,0 +1,35 @@
+package com.shayanr.HomeServiceSpring.entity.business;
+
+
+
+import com.shayanr.HomeServiceSpring.entity.users.Customer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String comment;
+
+
+    private Integer score=0;
+
+    @OneToOne(mappedBy = "comment")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Customer customer;
+
+}
