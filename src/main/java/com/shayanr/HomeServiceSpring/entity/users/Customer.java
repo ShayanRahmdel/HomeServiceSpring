74c54c75beba3a488350c6cb8ea420b9a498c11a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:20:35 +0330

    create Customer

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/users/Customer.java
@@ -0,0 +1,45 @@
+package com.shayanr.HomeServiceSpring.entity.users;
+
+
+
+
+import com.shayanr.HomeServiceSpring.entity.business.Address;
+import com.shayanr.HomeServiceSpring.entity.business.Comment;
+import com.shayanr.HomeServiceSpring.entity.business.Wallet;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Customer extends User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Comment> comments;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Wallet wallet;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Address> addresses;
+
+    @Override
+    public String toString() {
+        return "Customer id " + getId()+"\n"+
+                "Customer name " + getFirstName()+" "+ getLastName()+"\n"+
+                "Customer email " + getEmail() +"\n"+
+                "Customer password " + getPassword();
+    }
+}
