fc1606227b92be0d82fc212255982a0c573abd88
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:24:11 +0330

    create SubDutyRepository

---
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/SubDutyRepository.java
@@ -0,0 +1,8 @@
+package com.shayanr.HomeServiceSpring.repositoy;
+
+
+import com.example.homeservicespringdata.entity.business.SubDuty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubDutyRepository extends JpaRepository<SubDuty,Integer> {
+}
