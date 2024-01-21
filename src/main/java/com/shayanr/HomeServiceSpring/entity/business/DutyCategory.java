ccf4f3d3fb017a0a86c6dc1b2d045eb65c008469
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-21 15:21:28 +0330

    create DutyCategory

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/business/DutyCategory.java
@@ -0,0 +1,39 @@
+package com.shayanr.HomeServiceSpring.entity.business;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DutyCategory  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = false,unique = true)
+    private String title;
+
+    @OneToMany(mappedBy = "dutyCategory")
+    private List<SubDuty> subDuties;
+
+
+
+    @Override
+    public String toString() {
+        return " DutyCategory id " + getId()+"\n" +
+                "  title " + title+"\n"+
+                "==================";
+    }
+
+
+}
