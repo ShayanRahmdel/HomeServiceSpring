49a65a6e70b3a32600ab9fa40cc8749392cb9b06
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:06:24 +0330

    create ExpertRepository and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/ExpertRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/ExpertRepository.java
@@ -1,7 +1,7 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.users.Expert;
+import com.shayanr.HomeServiceSpring.entity.users.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpertRepository extends JpaRepository<Expert,Integer> {
