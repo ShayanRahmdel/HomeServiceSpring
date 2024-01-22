7c84e1db55f395bd3cd2fc6f4f09c6f9075d3e28
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:07:41 +0330

    create OrderServiceImpl and impl last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/OrderServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/OrderServiceImpl.java
@@ -1,9 +1,56 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.Order;
+import com.shayanr.HomeServiceSpring.repositoy.OrderRepository;
+import com.shayanr.HomeServiceSpring.service.OrderService;
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class OrderServiceImpl {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    @Transactional
+    public Order saveOrder(Order order) {
+        try {
+            orderRepository.save(order);
+        }catch (NullPointerException | PersistenceException e){
+            System.out.println("Error saving order");
+        }
+        return order;
+    }
+
+    @Override
+    public Order findById(Integer orderId) {
+        if (orderId==null){
+            throw new NullPointerException("cant find order");
+        }
+        return orderRepository.findById(orderId).orElse(null);
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Integer orderId) {
+        if (orderId==null){
+            throw new NullPointerException("cant delete order");
+        }
+        orderRepository.deleteById(orderId);
+    }
+
+    @Override
+    public List<Order> seeOrders(Integer expertId) {
+        if (expertId==null){
+            throw new NullPointerException("cant find order");
+        }
+       return orderRepository.seeOrders(expertId);
+    }
 }
