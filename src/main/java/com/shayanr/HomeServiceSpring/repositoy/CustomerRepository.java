4094f7cef49cb06b9e3a1c91bb7c939b46a58061
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:04:42 +0330

    create CustomerRepository and copy last project methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
@@ -1,10 +1,14 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.users.Customer;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
+import com.shayanr.HomeServiceSpring.entity.users.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-
 
 }
