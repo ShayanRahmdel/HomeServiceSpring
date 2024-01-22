96c1ca4c1c600583de9a042d0d4c36f6aaf0103d
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:07:19 +0330

    create OrderRepository and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/OrderRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/OrderRepository.java
@@ -1,8 +1,18 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.Order;
+import com.shayanr.HomeServiceSpring.entity.business.Order;
+import com.shayanr.HomeServiceSpring.entity.enumration.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+    @Query("SELECT o FROM Order o " +
+            "JOIN o.subDuty sd " +
+            "JOIN sd.experts e " +
+            "WHERE e.id = :expertId AND o.orderStatus = 'WAITING_EXPERT_SUGESTION' OR o.orderStatus = 'WAITING_EXPERT_SELECTION'")
+    List<Order> seeOrders(@Param("expertId") Integer expertId);
 }
