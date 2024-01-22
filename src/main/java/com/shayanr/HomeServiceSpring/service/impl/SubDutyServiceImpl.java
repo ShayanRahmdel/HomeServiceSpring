6ac8a2aceec33bbb52ad0eb26944ef2b44432a1d
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:08:59 +0330

    create SubDutyServiceImpl and impl last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/SubDutyServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/SubDutyServiceImpl.java
@@ -4,24 +4,64 @@ package com.shayanr.HomeServiceSpring.service.impl;
 
 import com.shayanr.HomeServiceSpring.entity.business.SubDuty;
 import com.shayanr.HomeServiceSpring.repositoy.SubDutyRepository;
+import com.shayanr.HomeServiceSpring.service.SubDutyService;
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
-public class SubDutyServiceImpl {
+public class SubDutyServiceImpl implements SubDutyService {
 
-    private SubDutyRepository subDutyRepository;
+    private final SubDutyRepository subDutyRepository;
 
 
+    @Override
+    @Transactional
+    public SubDuty save(SubDuty subDuty) {
+        try {
+            subDutyRepository.save(subDuty);
+        }catch (NullPointerException | PersistenceException e){
+            System.out.println("Error saving subDuty");
+        }
+        return subDuty;
+    }
 
+    @Override
+    @Transactional
+    public void deleteById(Integer subDutyId) {
+        if (subDutyId==null){
+            throw new NullPointerException("cant find subduty");
+        }
+        subDutyRepository.deleteById(subDutyId);
+    }
+
+    @Override
+    public SubDuty findById(Integer subDutyId) {
+        if (subDutyId==null){
+            throw new NullPointerException("cant find subduty");
+        }
+        return subDutyRepository.findById(subDutyId).orElse(null);
+
+
+    }
+
+    @Override
+    public List<SubDuty> findAll() {
+       return subDutyRepository.findAll();
+    }
+
+    @Override
     public List<SubDuty> seeSubDutyByCategory(Integer category) {
         List<SubDuty> subDutyList = new ArrayList<>();
-        Collection<SubDuty> allSubDuty = repository.findAll();
+        Collection<SubDuty> allSubDuty = subDutyRepository.findAll();
         for (SubDuty subDuty : allSubDuty){
             if (Objects.equals(subDuty.getDutyCategory().getId(), category)){
                 subDutyList.add(subDuty);
@@ -29,5 +69,6 @@ public class SubDutyServiceImpl {
 
         }
         return subDutyList;
+
     }
 }
