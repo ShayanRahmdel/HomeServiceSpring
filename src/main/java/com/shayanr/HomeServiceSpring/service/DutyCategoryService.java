033865a1cc6d2689f7015f29fe4265765c8df5a4
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:05:34 +0330

    create DutyCategoryRepository and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/DutyCategoryService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/DutyCategoryService.java
@@ -1,8 +1,18 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.DutyCategory;
+
+import java.util.List;
 
 public interface DutyCategoryService {
 
+    DutyCategory save(DutyCategory category);
+
+    DutyCategory findById(Integer dutyCategoryId);
+
+    void deleteById(Integer dutyCategoryId);
+
+    List<DutyCategory> findAll();
     void updateDutyCategory(Integer dutyCategoryId,String newTitle);
 }
