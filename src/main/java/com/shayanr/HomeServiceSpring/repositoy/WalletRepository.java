bc27643ff3c7e3b4b6593910ed29d27b54747f05
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:09:35 +0330

    create WalletRepository

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/WalletRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/WalletRepository.java
@@ -1,7 +1,7 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.Wallet;
+import com.shayanr.HomeServiceSpring.entity.business.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletRepository extends JpaRepository<Wallet,Integer> {
