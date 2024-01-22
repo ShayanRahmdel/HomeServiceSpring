92ef5f619dfde9e27569ecd000e6c3221c976404
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:03:32 +0330

    create CommentRepository

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/CommentRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.Comment;
+import com.shayanr.HomeServiceSpring.entity.business.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 }
