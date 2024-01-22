763899047f35cdf079bcebd5210de359daa18147
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:08:41 +0330

    create SubDutyRepository and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/SubDutyRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/SubDutyRepository.java
@@ -1,7 +1,7 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.SubDuty;
+import com.shayanr.HomeServiceSpring.entity.business.SubDuty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubDutyRepository extends JpaRepository<SubDuty,Integer> {
