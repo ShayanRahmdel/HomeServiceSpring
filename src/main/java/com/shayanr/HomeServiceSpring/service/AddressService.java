2d4c964c004f7dfb370f530defa9fc388270a450
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:02:07 +0330

    create AddressService

--- src/main/java/com/shayanr/HomeServiceSpring/service/AddressService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/AddressService.java
@@ -1,6 +1,10 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.Address;
 
 public interface AddressService  {
+
+
+    Address saveAddress(Address address);
 }
