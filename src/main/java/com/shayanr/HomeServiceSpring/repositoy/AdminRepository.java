c9250472bbcfe780161ce4ce2d3f25984ff4a552
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:02:33 +0330

    create AdminRepository

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/AdminRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/AdminRepository.java
@@ -1,7 +1,7 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.users.Admin;
+import com.shayanr.HomeServiceSpring.entity.users.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
