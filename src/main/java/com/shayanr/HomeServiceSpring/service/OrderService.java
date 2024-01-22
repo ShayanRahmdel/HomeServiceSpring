60bbe320bc6cd2d4c7ad59ba3c263df716a5bee7
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:07:30 +0330

    create OrderService and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/OrderService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/OrderService.java
@@ -1,6 +1,19 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.Order;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderService {
+
+    Order saveOrder(Order order);
+
+    Order findById(Integer orderId);
+
+    void deleteById(Integer orderId);
+
+    List<Order> seeOrders(Integer expertId);
+
 }
