d3da5c4127c36f82a17c615e4c096960ca0ce24e
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:10:23 +0330

    create WorkSuggestionRepository and copy last project method and new method

--- src/main/java/com/shayanr/HomeServiceSpring/service/WorkSuggestionService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/WorkSuggestionService.java
@@ -1,6 +1,18 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
+
+import java.util.List;
 
 public interface WorkSuggestionService {
+
+    WorkSuggestion save(WorkSuggestion workSuggestion);
+
+    void deleteById(Integer workSuggestionId);
+
+
+    List<WorkSuggestion> seeSuggestions(Integer customerId);
+
+
 }
