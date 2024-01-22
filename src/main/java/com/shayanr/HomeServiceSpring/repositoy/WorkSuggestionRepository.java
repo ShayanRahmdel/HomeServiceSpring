56466a43c578675f80078a481e51e1aaf593be27
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:10:00 +0330

    create WorkSuggestionRepository

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/WorkSuggestionRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/WorkSuggestionRepository.java
@@ -1,8 +1,15 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.WorkSuggestion;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WorkSuggestionRepository extends JpaRepository<WorkSuggestion,Integer> {
+
+    @Query("SELECT w FROM WorkSuggestion w JOIN w.order o WHERE o.customer.id = :customerId")
+    List<WorkSuggestion> seeWorkSuggestions(@Param("customerId")Integer customerId);
 }
