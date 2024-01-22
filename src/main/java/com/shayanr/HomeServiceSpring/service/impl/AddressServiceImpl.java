293ea00199d8deabb8635da643438665d238ebd8
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:02:17 +0330

    create AddressServiceImpl

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/AddressServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/AddressServiceImpl.java
@@ -1,13 +1,29 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
+import com.shayanr.HomeServiceSpring.entity.business.Address;
 import com.shayanr.HomeServiceSpring.repositoy.AddressRepository;
+import com.shayanr.HomeServiceSpring.service.AddressService;
+import com.shayanr.HomeServiceSpring.service.AdminService;
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AddressServiceImpl {
-    private AddressRepository addressRepository;
+public class AddressServiceImpl implements AddressService {
+    private final AddressRepository addressRepository;
 
+
+    @Override
+    @Transactional
+    public Address saveAddress(Address address) {
+        try {
+            addressRepository.save(address);
+        }catch (NullPointerException | PersistenceException e){
+            System.out.println("Error saving address");
+        }
+        return address;
+    }
 
 }
