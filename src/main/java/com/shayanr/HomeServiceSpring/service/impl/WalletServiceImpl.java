78647afb7f6c5c44af01d26f698f4ae38679692f
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:09:44 +0330

    create WalletServiceImpl

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/WalletServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/WalletServiceImpl.java
@@ -1,11 +1,12 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
 import com.shayanr.HomeServiceSpring.repositoy.WalletRepository;
+import com.shayanr.HomeServiceSpring.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class WalletServiceImpl {
-    private WalletRepository walletRepository;
+public class WalletServiceImpl implements WalletService {
+    private final WalletRepository walletRepository;
 }
