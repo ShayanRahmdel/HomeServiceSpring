727b89450c28747d5515e89895bf0902ace3938a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:06:32 +0330

    create ExpertService and copy last project  methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/ExpertService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/ExpertService.java
@@ -3,21 +3,34 @@ package com.shayanr.HomeServiceSpring.service;
 
 
 import com.shayanr.HomeServiceSpring.entity.business.Order;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import com.shayanr.HomeServiceSpring.entity.users.Expert;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ExpertService {
 
+    Expert save(Expert expert);
+
+    Expert findById(Integer expertId);
+
+    void deleteById(Integer expertId);
+
+    List<Expert> findAll();
+
     Expert signUp(Expert expert, File image);
 
-    void changePassword(String email, String oldPassword,String newPassword);
+    void changePassword(Integer expertId,String newPassword,String confirmPassword);
 
     List<Order> seeOrder(Integer expertId);
 
-    Order acceptOrder(Integer orderId);
+
 
     void saveImage(String imagePath,Integer expertId);
+
+    WorkSuggestion createSuggest(WorkSuggestion workSuggestion, Integer orderId, Integer expertId);
 
 }
