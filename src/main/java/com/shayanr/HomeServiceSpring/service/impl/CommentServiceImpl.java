5cdd01ca33be40d3b52fb241750e0a35404252b7
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:03:40 +0330

    create CommentServiceImpl

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/CommentServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/CommentServiceImpl.java
@@ -1,11 +1,12 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
 import com.shayanr.HomeServiceSpring.repositoy.CommentRepository;
+import com.shayanr.HomeServiceSpring.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CommentServiceImpl {
-    private CommentRepository commentRepository;
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
 }
