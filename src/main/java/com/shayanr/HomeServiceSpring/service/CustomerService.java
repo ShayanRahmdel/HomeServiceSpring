0af729e99f3e24f0d7b4f9da5850655a379741a6
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:04:50 +0330

    create CustomerService and copy last project methods and new methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/CustomerService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/CustomerService.java
@@ -3,16 +3,28 @@ package com.shayanr.HomeServiceSpring.service;
 
 import com.shayanr.HomeServiceSpring.entity.business.Address;
 import com.shayanr.HomeServiceSpring.entity.business.Order;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import com.shayanr.HomeServiceSpring.entity.users.Customer;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerService {
 
+    Optional<Customer> findById(Integer customerId);
+
+    void deleteById(Integer customerId);
+
+    List<Customer> findAll();
+
     Customer signUp(Customer customer);
 
-    void changePassword(String email, String oldPassword,String newPassword);
+    void changePassword(Integer customerId,String newPassword,String confirmPassword);
 
     Order createOrder(Order order, Integer category, Integer subDutyId, Integer customerId);
 
     Address createAddress(Address address, Integer customerId, Integer orderId);
+
+    List<WorkSuggestion> seeWorkSuggestions(Integer customerId);
 
 }
