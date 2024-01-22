da032885867e4d4af6fe8da27a1eccdbc835cf1c
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-22 21:01:59 +0330

    create AddressRepository

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/AddressRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/AddressRepository.java
@@ -1,7 +1,7 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
 
-import com.example.homeservicespringdata.entity.business.Address;
+import com.shayanr.HomeServiceSpring.entity.business.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address,Integer> {
