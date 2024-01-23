175063f51122a7dd68ca80e16f0ac53a67955f5d
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-23 11:26:26 +0330

    write methods seeSuggestionByPrice and Score with query

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
@@ -10,5 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    @Query("SELECT w FROM WorkSuggestion w JOIN w.order o WHERE o.customer.id = :customerId order by w.suggestedPrice")
+    List<WorkSuggestion> seeSuggestionsByPrice(@Param("customerId")Integer customerId);
 
+
+    @Query("SELECT w FROM WorkSuggestion w JOIN w.order o JOIN w.expert e WHERE o.customer.id = :customerId ORDER BY e.overallScore DESC")
+    List<WorkSuggestion> seeSuggestionsByExpertScore(@Param("customerId") Integer customerId);
 }
