d8487f1f79377508713f0fd6d125807d2751557e
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-23 11:28:44 +0330

    add findById

--- src/main/java/com/shayanr/HomeServiceSpring/service/WorkSuggestionService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/WorkSuggestionService.java
@@ -4,15 +4,19 @@ package com.shayanr.HomeServiceSpring.service;
 import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkSuggestionService {
 
     WorkSuggestion save(WorkSuggestion workSuggestion);
 
+
+    Optional<WorkSuggestion> findById(Integer workSuggestionId);
+
     void deleteById(Integer workSuggestionId);
 
 
-    List<WorkSuggestion> seeSuggestions(Integer customerId);
+
 
 
 }
