6d1399cd3241a8a1aa4e0b8aaaae98da65f60f39
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-27 21:56:59 +0330

    change return type and add deleteall

--- src/main/java/com/shayanr/HomeServiceSpring/service/ExpertService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/ExpertService.java
@@ -2,13 +2,13 @@ package com.shayanr.HomeServiceSpring.service;
 
 
 
-import com.shayanr.HomeServiceSpring.entity.business.Order;
+import com.shayanr.HomeServiceSpring.entity.business.CustomerOrder;
 import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import com.shayanr.HomeServiceSpring.entity.users.Expert;
 
 import java.io.File;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public interface ExpertService {
@@ -21,16 +21,17 @@ public interface ExpertService {
 
     List<Expert> findAll();
 
-    Expert signUp(Expert expert, File image);
+    Expert signUp(Expert expert, File image) throws IOException;
 
-    void changePassword(Integer expertId,String newPassword,String confirmPassword);
+    Expert changePassword(Integer expertId,String newPassword,String confirmPassword);
 
-    List<Order> seeOrder(Integer expertId);
+    List<CustomerOrder> seeOrder(Integer expertId);
 
-
-
-    void saveImage(String imagePath,Integer expertId);
+    void saveImage(String imagePath,Integer expertId) throws IOException;
 
     WorkSuggestion createSuggest(WorkSuggestion workSuggestion, Integer orderId, Integer expertId);
+
+
+    void deleteAll();
 
 }
