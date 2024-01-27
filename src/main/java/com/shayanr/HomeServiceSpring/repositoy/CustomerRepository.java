3881227b07c0d98c7818763427b49c8750bee383
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-27 21:54:56 +0330

    refactor queries

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/CustomerRepository.java
@@ -10,10 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-    @Query("SELECT w FROM WorkSuggestion w JOIN w.order o WHERE o.customer.id = :customerId order by w.suggestedPrice")
+    @Query("SELECT w FROM WorkSuggestion w JOIN w.customerOrder o WHERE o.customer.id = :customerId order by w.suggestedPrice ASC ")
     List<WorkSuggestion> seeSuggestionsByPrice(@Param("customerId")Integer customerId);
 
 
-    @Query("SELECT w FROM WorkSuggestion w JOIN w.order o JOIN w.expert e WHERE o.customer.id = :customerId ORDER BY e.overallScore DESC")
+    @Query("SELECT w FROM WorkSuggestion w JOIN w.customerOrder o JOIN w.expert e WHERE o.customer.id = :customerId ORDER BY e.overallScore DESC")
     List<WorkSuggestion> seeSuggestionsByExpertScore(@Param("customerId") Integer customerId);
 }
