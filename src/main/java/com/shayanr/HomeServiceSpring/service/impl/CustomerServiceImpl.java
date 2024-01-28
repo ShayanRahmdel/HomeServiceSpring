ef2c990e6ba47dbd602150b2753eaaaffbf1f462
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-28 10:51:56 +0330

    refactor method and remove try catch

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/CustomerServiceImpl.java
@@ -55,78 +55,56 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<Customer> findAll() {
-      return   customerRepository.findAll();
+        return customerRepository.findAll();
     }
 
     @Override
     @Transactional
     public Customer signUp(Customer customer) {
-//        try {
-            if (!Validate.nameValidation(customer.getFirstName()) ||
-                    !Validate.nameValidation(customer.getLastName()) ||
-                    !Validate.emailValidation(customer.getEmail()) ||
-                    !Validate.passwordValidation(customer.getPassword())) {
-                throw new IllegalArgumentException();
-            }
-            customerRepository.save(customer);
-            return customer;
-//        }catch ( NullPointerException e){
-//            System.out.println("Error saving customer");
-//        }
-//        return customer;
 
-    }
-
-    @Override
-    @Transactional
-    public Customer changePassword(Integer customerId,String newPassword,String confirmPassword) {
-        Customer customer = customerRepository.findById(customerId).orElse(null);
-
-        try {
-            if (customer == null){
-                throw new NullPointerException("customerId not found");
-            }
-
-            if (!Validate.passwordValidation(newPassword) || !newPassword.equals(confirmPassword)){
-                throw new PersistenceException("Password not valid");
-            }
-            customer.setPassword(newPassword);
-            customerRepository.save(customer);
-            return customer;
-        }catch (NullPointerException | PersistenceException e){
-            System.out.println(e.getMessage());
+        if (!Validate.nameValidation(customer.getFirstName()) ||
+                !Validate.nameValidation(customer.getLastName()) ||
+                !Validate.emailValidation(customer.getEmail()) ||
+                !Validate.passwordValidation(customer.getPassword())) {
+            throw new IllegalArgumentException();
         }
+        customerRepository.save(customer);
         return customer;
 
     }
 
     @Override
     @Transactional
+    public Customer changePassword(Integer customerId, String newPassword, String confirmPassword) {
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new NullPointerException("Could not find customer"));
+        if (!Validate.passwordValidation(newPassword) || !newPassword.equals(confirmPassword)) {
+            throw new PersistenceException("Password not valid");
+        }
+        customer.setPassword(newPassword);
+        customerRepository.save(customer);
+        return customer;
+    }
+
+    @Override
+    @Transactional
     public CustomerOrder createOrder(CustomerOrder customerOrder, Integer category, Integer subDutyId, Integer customerId) {
         SubDuty subDuty = subDutyService.findById(subDutyId);
-        Customer customer = customerRepository.findById(customerId).orElse(null);
-        try {
-            if (dutyCategoryService.findById(category) == null){
-                throw new NullPointerException();
-            }
-
-            if (!Validate.isValidDateAndTime(customerOrder.getWorkDate(), customerOrder.getTimeDate())) {
-                throw new PersistenceException();
-            }
-                if (!isValidPrice(subDuty, customerOrder.getProposedPrice())) {
-                    throw new PersistenceException();
-                }
-
-            customerOrder.setSubDuty(subDuty);
-            customerOrder.setCustomer(customer);
-            customerOrder.setOrderStatus(OrderStatus.WAITING_EXPERT_SUGESTION);
-            orderService.saveOrder(customerOrder);
-
-        }catch (PersistenceException | NullPointerException e) {
-            System.out.println("Error: saving order:");
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new NullPointerException("Could not find customer"));
+        if (dutyCategoryService.findById(category) == null) {
+            throw new NullPointerException();
         }
-        return customerOrder;
 
+        if (!Validate.isValidDateAndTime(customerOrder.getWorkDate(), customerOrder.getTimeDate())) {
+            throw new PersistenceException();
+        }
+        if (!isValidPrice(subDuty, customerOrder.getProposedPrice())) {
+            throw new PersistenceException();
+        }
+
+        customerOrder.setSubDuty(subDuty);
+        customerOrder.setCustomer(customer);
+        customerOrder.setOrderStatus(OrderStatus.WAITING_EXPERT_SUGESTION);
+        return orderService.saveOrder(customerOrder);
     }
 
     @Override
@@ -134,93 +112,70 @@ public class CustomerServiceImpl implements CustomerService {
     public Address createAddress(Address address, Integer customerId, Integer orderId) {
         Customer customer = customerRepository.findById(customerId).orElse(null);
         CustomerOrder customerOrder = orderService.findById(orderId);
-        try {
-            if (!Validate.cityValidation(address.getCity()) || !Validate.postalValidation(address.getPostalCode())
-                    || !Validate.isValidCity(address.getState())) {
-                throw new PersistenceException("not a valid address");
-            }
-            address.setCustomer(customer);
-            addressService.saveAddress(address);
-
-            assert customerOrder != null;
-            customerOrder.setAddress(address);
-            orderService.saveOrder(customerOrder);
-            return address;
-        }catch (PersistenceException | NullPointerException e){
-            System.out.println(e.getMessage());
+        if (!Validate.cityValidation(address.getCity()) || !Validate.postalValidation(address.getPostalCode())
+                || !Validate.isValidCity(address.getState())) {
+            throw new PersistenceException("not a valid address");
         }
+        address.setCustomer(customer);
+        addressService.saveAddress(address);
+
+        assert customerOrder != null;
+        customerOrder.setAddress(address);
+        orderService.saveOrder(customerOrder);
         return address;
+
     }
 
     @Override
     public List<WorkSuggestion> seeSuggestionsByPrice(Integer customerId) {
-        try {
-            return customerRepository.seeSuggestionsByPrice(customerId);
-        }catch (NullPointerException | InvalidDataAccessApiUsageException e){
-            System.out.println("Error:");
-        }
-        return null;
 
+        customerRepository.findById(customerId).orElseThrow(() -> new NullPointerException("Customer not found"));
+        return customerRepository.seeSuggestionsByPrice(customerId);
     }
 
     @Override
     public List<WorkSuggestion> seeSuggestionsByExpertScore(Integer customerId) {
-        try {
-            return customerRepository.seeSuggestionsByExpertScore(customerId);
-        }catch (NullPointerException | InvalidDataAccessApiUsageException e){
-            System.out.println("Error:");
-        }
-        return null;
+
+        customerRepository.findById(customerId).orElseThrow(() -> new NullPointerException("Customer not found"));
+        return customerRepository.seeSuggestionsByExpertScore(customerId);
+
     }
 
     @Override
     @Transactional
     public CustomerOrder acceptSuggest(Integer suggestId) {
-        try {
-            WorkSuggestion workSuggestion = workSuggestionService.findById(suggestId);
-            assert workSuggestion != null;
-            CustomerOrder customerOrder = workSuggestion.getCustomerOrder();
-            customerOrder.setOrderStatus(OrderStatus.WAITING_FOR_THE_SPECIALIST_TO_COME);
-            orderService.saveOrder(customerOrder);
-            return customerOrder;
-        }catch (NullPointerException | IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
+        WorkSuggestion workSuggestion = workSuggestionService.findById(suggestId);
+        assert workSuggestion != null;
+        CustomerOrder customerOrder = workSuggestion.getCustomerOrder();
+        customerOrder.setOrderStatus(OrderStatus.WAITING_FOR_THE_SPECIALIST_TO_COME);
+        orderService.saveOrder(customerOrder);
+        return customerOrder;
+
     }
 
     @Override
     @Transactional
     public CustomerOrder updateOrderToBegin(Integer orderId, Integer suggestionId, LocalDate date) {
-        try {
-            CustomerOrder customerOrder = orderService.findById(orderId);
-            WorkSuggestion workSuggestion = workSuggestionService.findById(suggestionId);
+        CustomerOrder customerOrder = orderService.findById(orderId);
+        WorkSuggestion workSuggestion = workSuggestionService.findById(suggestionId);
 
-            assert workSuggestion != null;
-            if (date.isBefore(workSuggestion.getSuggestedDate())){
-                throw new IllegalArgumentException("Date is before " + workSuggestion.getSuggestedDate());
-            }
-            customerOrder.setOrderStatus(OrderStatus.WORK_BEGIN);
-            orderService.saveOrder(customerOrder);
-            return customerOrder;
-        }catch (NullPointerException | IllegalArgumentException e){
-            System.out.println(e.getMessage());
+        assert workSuggestion != null;
+        if (date.isBefore(workSuggestion.getSuggestedDate())) {
+            throw new IllegalArgumentException("Date is before " + workSuggestion.getSuggestedDate());
         }
-    return null;
+        customerOrder.setOrderStatus(OrderStatus.WORK_BEGIN);
+        orderService.saveOrder(customerOrder);
+        return customerOrder;
+
     }
 
     @Override
     @Transactional
     public CustomerOrder updateOrderToEnd(Integer orderId) {
-        try {
-            CustomerOrder customerOrder = orderService.findById(orderId);
-            customerOrder.setOrderStatus(OrderStatus.WORK_DONE);
-            orderService.saveOrder(customerOrder);
-            return customerOrder;
-        }catch (NullPointerException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
+        CustomerOrder customerOrder = orderService.findById(orderId);
+        customerOrder.setOrderStatus(OrderStatus.WORK_DONE);
+        orderService.saveOrder(customerOrder);
+        return customerOrder;
     }
 
     @Override
@@ -230,9 +185,9 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
 
-    boolean isValidPrice(SubDuty subDuty, Double price){
+    boolean isValidPrice(SubDuty subDuty, Double price) {
         Double basePrice = subDuty.getBasePrice();
-        if (price< basePrice){
+        if (price < basePrice) {
             System.out.println("Your price is less than BasePrice");
             return false;
         }
