3067621f2ddbc3f0137dab6bf32f4ae3e6a487ce
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-04 12:18:18 +0330

    add save and findbyid

--- src/main/java/com/shayanr/HomeServiceSpring/service/CommentService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/CommentService.java
@@ -1,5 +1,14 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.Comment;
+
+import java.util.Optional;
+
 public interface CommentService {
+
+    Comment save(Comment comment);
+
+
+    Optional<Comment> findById(Integer id);
 }
