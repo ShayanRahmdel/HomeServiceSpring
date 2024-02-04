bb0b40a5b2ef5077d5193b1bc2aae5a8da00ae85
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-04 12:22:03 +0330

    add save and findById

--- src/main/java/com/shayanr/HomeServiceSpring/service/WalletService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/WalletService.java
@@ -1,6 +1,15 @@
 package com.shayanr.HomeServiceSpring.service;
 
 
+import com.shayanr.HomeServiceSpring.entity.business.Wallet;
+
+import java.util.Optional;
 
 public interface WalletService{
+
+    Wallet save(Wallet wallet);
+
+    Optional<Wallet> findById(Integer id);
+
+
 }
