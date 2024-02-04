b89bf7bdae7e97637baf5540c722d4030743121d
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-04 12:20:12 +0330

    change returntype to Optional

--- src/main/java/com/shayanr/HomeServiceSpring/service/OrderService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/OrderService.java
@@ -4,12 +4,13 @@ package com.shayanr.HomeServiceSpring.service;
 import com.shayanr.HomeServiceSpring.entity.business.CustomerOrder;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderService {
 
     CustomerOrder saveOrder(CustomerOrder customerOrder);
 
-    CustomerOrder findById(Integer orderId);
+    Optional<CustomerOrder> findById(Integer orderId);
 
     void deleteById(Integer orderId);
 
