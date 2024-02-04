19192fd9742f024d439b432ca69c34ebbf42fb71
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-04 12:22:22 +0330

    impl save and findById

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/WalletServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/WalletServiceImpl.java
@@ -1,12 +1,30 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
+import com.shayanr.HomeServiceSpring.entity.business.Wallet;
+import com.shayanr.HomeServiceSpring.exception.NotFoundException;
 import com.shayanr.HomeServiceSpring.repositoy.WalletRepository;
 import com.shayanr.HomeServiceSpring.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService {
     private final WalletRepository walletRepository;
+
+    @Override
+    public Wallet save(Wallet wallet) {
+        return walletRepository.save(wallet);
+    }
+
+    @Override
+    public Optional<Wallet> findById(Integer id) {
+        Optional<Wallet> wallet = walletRepository.findById(id);
+        if (wallet.isEmpty()){
+            throw new NotFoundException("Wallet not found");
+        }
+        return wallet;
+    }
 }
