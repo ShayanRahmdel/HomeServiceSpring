0c88a0fff425825aa76c685f8a06c13e458f19fb
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-04 12:18:26 +0330

    impl save and findbyid

--- src/main/java/com/shayanr/HomeServiceSpring/service/impl/CommentServiceImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/impl/CommentServiceImpl.java
@@ -1,12 +1,31 @@
 package com.shayanr.HomeServiceSpring.service.impl;
 
+import com.shayanr.HomeServiceSpring.entity.business.Comment;
+import com.shayanr.HomeServiceSpring.exception.NotFoundException;
 import com.shayanr.HomeServiceSpring.repositoy.CommentRepository;
 import com.shayanr.HomeServiceSpring.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
+
+    @Override
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public Optional<Comment> findById(Integer id) {
+        Optional<Comment> commentOptional = commentRepository.findById(id);
+        if (commentOptional.isEmpty()) {
+            throw new NotFoundException("Comment not found");
+        }
+        return commentOptional;
+    }
 }
+
