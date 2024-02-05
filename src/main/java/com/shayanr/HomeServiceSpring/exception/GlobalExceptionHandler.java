b8d558eca9ae08971bb05e6fd008a6af7a5b32ca
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-05 15:27:29 +0330

    Create GlobalExceptionHandler

---
+++ src/main/java/com/shayanr/HomeServiceSpring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.shayanr.HomeServiceSpring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleException(NotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<String> duplicateExceptionHandler(DuplicateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);  //409
+//        return ex.getMessage(); //200
+    }
+    @ExceptionHandler(IsEmptyFieldException.class)
+    public ResponseEntity<String> emptyFieldHandler(IsEmptyFieldException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> validationExceptionHandler(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+}
