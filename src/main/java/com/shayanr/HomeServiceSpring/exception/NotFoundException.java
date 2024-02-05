5670cb1e746b50150556956f877ded2418bc002a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-05 15:28:59 +0330

    create NotFoundException

---
+++ src/main/java/com/shayanr/HomeServiceSpring/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.shayanr.HomeServiceSpring.exception;
+
+//@ResponseStatus(value = HttpStatus.CONFLICT, reason = "this content not found")
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
