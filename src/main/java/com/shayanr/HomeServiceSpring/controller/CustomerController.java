e309d631aff8c6541af54cafbd60326513adff4e
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-05 18:42:16 +0330

    add other customerservice to controller

--- src/main/java/com/shayanr/HomeServiceSpring/controller/CustomerController.java
+++ src/main/java/com/shayanr/HomeServiceSpring/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.shayanr.HomeServiceSpring.dto.CustomerResponseDto;
 import com.shayanr.HomeServiceSpring.dto.PasswordDto;
 import com.shayanr.HomeServiceSpring.entity.business.Address;
 import com.shayanr.HomeServiceSpring.entity.business.CustomerOrder;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import com.shayanr.HomeServiceSpring.entity.users.Customer;
 import com.shayanr.HomeServiceSpring.mapper.CustomerMapper;
 import com.shayanr.HomeServiceSpring.service.CustomerService;
@@ -13,6 +14,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -43,5 +48,39 @@ public class CustomerController {
     public ResponseEntity<Address> createAddress(@RequestBody Address address, @PathVariable Integer customerId
     ,@PathVariable Integer orderId){
         return new ResponseEntity<>(customerService.createAddress(address,customerId,orderId),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/see-suggest-by-price/{customerId}")
+    public List<WorkSuggestion> seeSuggestByPrice(@PathVariable Integer customerId){
+        return customerService.seeSuggestionsByPrice(customerId);
+    }
+
+    @GetMapping("/see-suggest-by-score/{customerId}")
+    public List<WorkSuggestion> seeSuggestByScore(@PathVariable Integer customerId){
+        return customerService.seeSuggestionsByExpertScore(customerId);
+    }
+
+    @PutMapping("/accept-suggest/{suggestId}")
+    public void acceptSuggest(@PathVariable Integer suggestionId){
+        customerService.acceptSuggest(suggestionId);
+    }
+
+    @PutMapping("/update-to-begin/{orderId}/{suggestionId}")
+    public void updateToBegin(@PathVariable Integer orderId
+            ,@PathVariable Integer suggestionId
+            ,@RequestBody LocalDate date){
+        customerService.updateOrderToBegin(orderId, suggestionId, date);
+    }
+
+    @PutMapping("/update-to-end/{orderId}")
+    public void updateToEnd(@PathVariable Integer orderId){
+        customerService.updateOrderToEnd(orderId);
+    }
+
+    @PostMapping("/payment-by-wallet/{orderId}/{suggestionId}")
+    public ResponseEntity<CustomerOrder> payedByWallet(@RequestBody LocalTime doneTime,
+                                                       @PathVariable Integer orderId,
+                                                       @PathVariable Integer suggestionId){
+        return new ResponseEntity<>(customerService.paidByWallet(orderId, suggestionId,doneTime),HttpStatus.OK);
     }
 }
