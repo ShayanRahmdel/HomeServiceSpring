971f7468bfa9b3900bd5c6eea6ce2cf19397ce41
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-05 15:24:47 +0330

    add methods change password create order create address

--- src/main/java/com/shayanr/HomeServiceSpring/controller/CustomerController.java
+++ src/main/java/com/shayanr/HomeServiceSpring/controller/CustomerController.java
@@ -2,6 +2,9 @@ package com.shayanr.HomeServiceSpring.controller;
 
 import com.shayanr.HomeServiceSpring.dto.CustomerRequestDto;
 import com.shayanr.HomeServiceSpring.dto.CustomerResponseDto;
+import com.shayanr.HomeServiceSpring.dto.PasswordDto;
+import com.shayanr.HomeServiceSpring.entity.business.Address;
+import com.shayanr.HomeServiceSpring.entity.business.CustomerOrder;
 import com.shayanr.HomeServiceSpring.entity.users.Customer;
 import com.shayanr.HomeServiceSpring.mapper.CustomerMapper;
 import com.shayanr.HomeServiceSpring.service.CustomerService;
@@ -9,10 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
@@ -26,5 +26,22 @@ public class CustomerController {
         customerService.signUp(customer);
         CustomerResponseDto customerResponseDto = CustomerMapper.INSTANCE.modelToResponse(customer);
         return new ResponseEntity<>(customerResponseDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/change-password/{customerId}")
+    public void changePassword(@PathVariable Integer customerId, @RequestBody PasswordDto passwordDto){
+        customerService.changePassword(customerId, passwordDto.getNewPassword(), passwordDto.getConfirmPassword());
+    }
+
+    @PostMapping("/create-order/{category}/{subduty}/{customerId}")
+    public ResponseEntity<CustomerOrder> createOrder(@RequestBody CustomerOrder order, @PathVariable Integer category,
+                                                     @PathVariable Integer subduty,@PathVariable Integer customerId){
+        return new ResponseEntity<>(customerService.createOrder(order,category,subduty,customerId),HttpStatus.CREATED);
+    }
+
+    @PostMapping("/create-address/{customerId}/{orderId}")
+    public ResponseEntity<Address> createAddress(@RequestBody Address address, @PathVariable Integer customerId
+    ,@PathVariable Integer orderId){
+        return new ResponseEntity<>(customerService.createAddress(address,customerId,orderId),HttpStatus.CREATED);
     }
 }
