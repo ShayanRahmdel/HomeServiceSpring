cd3fd5bdb7cca46c4b441d508295bd5c2f8e70c4
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-22 15:21:19 +0330

    ADD Admin methods

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/AdminRepositoryCustom.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/AdminRepositoryCustom.java
@@ -1,13 +1,19 @@
 package com.shayanr.HomeServiceSpring.repositoy;
 
-import com.shayanr.HomeServiceSpring.entity.users.Customer;
-import com.shayanr.HomeServiceSpring.entity.users.Expert;
+import com.shayanr.HomeServiceSpring.entity.business.CustomerOrder;
+import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
+import com.shayanr.HomeServiceSpring.entity.enumration.OrderStatus;
+
 import com.shayanr.HomeServiceSpring.entity.users.User;
 
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AdminRepositoryCustom {
-    List<Expert> searchAdminByExpert(String name, String lastName, String email, String expertise, Double minRate, Double maxRate);
+    List<User> searchAdminByUser(String name, String lastName, String email, String expertise, Double minRate, Double maxRate);
+    List<WorkSuggestion> searchWorkSuggestionByName(String firstName, String lastName);
 
-    List<Customer> searchAdminByCustomer(String name, String lastName, String email);
+    List<CustomerOrder> searchOrders(LocalDate startDate, LocalDate endDate, OrderStatus orderStatus,
+                                     String category,String subDuty);
 }
