71eca525d6ca84897df3c038f88733832d37656c
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-22 15:22:55 +0330

    remove extra import

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/CommentRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/CommentRepository.java
@@ -3,7 +3,7 @@ package com.shayanr.HomeServiceSpring.repositoy;
 
 import com.shayanr.HomeServiceSpring.entity.business.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 }
