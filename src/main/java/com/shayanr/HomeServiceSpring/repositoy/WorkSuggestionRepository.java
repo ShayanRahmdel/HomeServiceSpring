102d40c90317f09884ffeab55d7f32f25ea29c43
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-22 15:40:46 +0330

    remove extra

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/WorkSuggestionRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/WorkSuggestionRepository.java
@@ -3,12 +3,8 @@ package com.shayanr.HomeServiceSpring.repositoy;
 
 import com.shayanr.HomeServiceSpring.entity.business.WorkSuggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface WorkSuggestionRepository extends JpaRepository<WorkSuggestion,Integer> {
-
 
 }
