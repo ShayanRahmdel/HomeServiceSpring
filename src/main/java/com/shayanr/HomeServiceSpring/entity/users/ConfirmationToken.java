1df4f0584c5c2a9e00bed5215de361a11d8f4516
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-22 15:23:11 +0330

    Create ConfirmationToken

---
+++ src/main/java/com/shayanr/HomeServiceSpring/entity/users/ConfirmationToken.java
@@ -0,0 +1,36 @@
+package com.shayanr.HomeServiceSpring.entity.users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer tokenId;
+
+    private String confirmationToken;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
+    }
+}
