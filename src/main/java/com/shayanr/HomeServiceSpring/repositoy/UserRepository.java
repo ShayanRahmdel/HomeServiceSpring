32306d0b777862404334f20cb8110d3f3327d95a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-22 15:40:11 +0330

    create UserRepository

---
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/UserRepository.java
@@ -0,0 +1,16 @@
+package com.shayanr.HomeServiceSpring.repositoy;
+
+
+import com.shayanr.HomeServiceSpring.entity.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
+
+
+}
