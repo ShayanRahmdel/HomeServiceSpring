0db6bbab561b70e38ae62db1a8a4fc8e5c31ebb7
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-28 19:35:23 +0330

    refactor method

--- src/main/java/com/shayanr/HomeServiceSpring/repositoy/UserRepository.java
+++ src/main/java/com/shayanr/HomeServiceSpring/repositoy/UserRepository.java
@@ -3,14 +3,17 @@ package com.shayanr.HomeServiceSpring.repositoy;
 
 import com.shayanr.HomeServiceSpring.entity.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 import java.util.Optional;
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository <T extends User> extends JpaRepository<T, Integer> {
 
-    Optional<User> findByEmail(String email);
+    Optional<T> findByEmail(String email);
+    @Query("select w.amount from User u join u.wallet w WHERE u.id=:id")
+    Double seeAmountWallet(Integer id);
 
 
 }
