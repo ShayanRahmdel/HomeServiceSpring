4870d0c3584d21bb99ab2a43736bb631a32700a8
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-28 19:33:48 +0330

    impl listmodelToresponse

--- src/main/java/com/shayanr/HomeServiceSpring/mapper/impl/ExpertMapperCustomImpl.java
+++ src/main/java/com/shayanr/HomeServiceSpring/mapper/impl/ExpertMapperCustomImpl.java
@@ -3,6 +3,7 @@ package com.shayanr.HomeServiceSpring.mapper.impl;
 import com.shayanr.HomeServiceSpring.dto.ExpertRequestDto;
 import com.shayanr.HomeServiceSpring.dto.ExpertResponseCustomDto;
 import com.shayanr.HomeServiceSpring.dto.ExpertResponseDto;
+import com.shayanr.HomeServiceSpring.entity.business.SubDuty;
 import com.shayanr.HomeServiceSpring.entity.users.Expert;
 import com.shayanr.HomeServiceSpring.mapper.ExpertMapperCustom;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class ExpertMapperCustomImpl implements ExpertMapperCustom {
@@ -21,12 +23,12 @@ public class ExpertMapperCustomImpl implements ExpertMapperCustom {
         }
         Expert expert = new Expert();
 
-        expert.setFirstName( requestDto.getFirstName() );
-        expert.setLastName( requestDto.getLastName() );
-        expert.setEmail( requestDto.getEmail() );
-        expert.setPassword( requestDto.getPassword() );
-        expert.setSignUpDate( requestDto.getSignUpDate() );
-        expert.setSignUpTime( requestDto.getSignUpTime() );
+        expert.setFirstName(requestDto.getFirstName());
+        expert.setLastName(requestDto.getLastName());
+        expert.setEmail(requestDto.getEmail());
+        expert.setPassword(requestDto.getPassword());
+        expert.setSignUpDate(requestDto.getSignUpDate());
+        expert.setSignUpTime(requestDto.getSignUpTime());
         expert.setImage(requestDto.getImage().getBytes());
 
         return expert;
@@ -34,29 +36,36 @@ public class ExpertMapperCustomImpl implements ExpertMapperCustom {
 
     @Override
     public ExpertResponseDto modelToResponse(Expert expert) {
-        if ( expert == null ) {
+        if (expert == null) {
             return null;
         }
 
         ExpertResponseDto expertResponseDto = new ExpertResponseDto();
 
-        expertResponseDto.setFirstName( expert.getFirstName() );
-        expertResponseDto.setLastName( expert.getLastName() );
-        expertResponseDto.setEmail( expert.getEmail() );
-        expertResponseDto.setOverallScore( expert.getOverallScore() );
+        expertResponseDto.setFirstName(expert.getFirstName());
+        expertResponseDto.setLastName(expert.getLastName());
+        expertResponseDto.setEmail(expert.getEmail());
+        expertResponseDto.setOverallScore(expert.getOverallScore());
+        Set<SubDuty> subDuties = expert.getSubDuties();
+        if (subDuties!=null){
+            for (SubDuty sub:subDuties) {
+                expertResponseDto.setSubDutyTitle(sub.getTitle());
+            }
+        }
+
 
         return expertResponseDto;
     }
 
     @Override
     public List<ExpertResponseDto> listModelToResponse(List<Expert> experts) {
-        if ( experts == null ) {
+        if (experts == null) {
             return null;
         }
 
-        List<ExpertResponseDto> list = new ArrayList<>( experts.size() );
-        for ( Expert expert : experts ) {
-            list.add( modelToResponse( expert ) );
+        List<ExpertResponseDto> list = new ArrayList<>(experts.size());
+        for (Expert expert : experts) {
+            list.add(modelToResponse(expert));
         }
 
         return list;
@@ -64,7 +73,7 @@ public class ExpertMapperCustomImpl implements ExpertMapperCustom {
 
     @Override
     public ExpertResponseCustomDto modelToResponseCustom(Expert expert) {
-        if ( expert == null ) {
+        if (expert == null) {
             return null;
         }
         ExpertResponseCustomDto dto = new ExpertResponseCustomDto();
@@ -74,5 +83,19 @@ public class ExpertMapperCustomImpl implements ExpertMapperCustom {
         dto.setOverallScore(expert.getOverallScore());
 
         return dto;
+    }
+
+    @Override
+    public List<ExpertResponseCustomDto> listModelToResponseCustom(List<Expert> experts) {
+        if (experts == null) {
+            return null;
+        }
+
+        List<ExpertResponseCustomDto> list = new ArrayList<>(experts.size());
+        for (Expert expert : experts) {
+            list.add(modelToResponseCustom(expert));
+        }
+
+        return list;
     }
 }
