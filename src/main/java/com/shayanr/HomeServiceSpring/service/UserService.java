4387783ac5fde48b5a6a9302527a379ecde63f35
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-02-28 19:35:34 +0330

    refactor methods

--- src/main/java/com/shayanr/HomeServiceSpring/service/UserService.java
+++ src/main/java/com/shayanr/HomeServiceSpring/service/UserService.java
@@ -2,13 +2,23 @@ package com.shayanr.HomeServiceSpring.service;
 
 import com.shayanr.HomeServiceSpring.entity.users.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public interface UserService {
+public interface UserService<T extends User> extends UserDetailsService {
 
-    Optional<User> findByEmail(String email);
+    Optional<T> findByEmail(String email);
+
+    void save(T t);
+
+
+    void sendEmail(String emailAddress);
+
+    ResponseEntity<?> confirmEmail(String confirmationToken);
+
+    Double seeAmountWallet(Integer id);
 
 }
